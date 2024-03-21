c798c3cc4d7c6a0c073497053ee50638241e9f5f
demetriex <eujohnmagno@gmail.com>
2024-03-21 09:24:35 +0800

    spawn obstacles

--- src/game.gd
+++ src/game.gd
@@ -1,14 +1,21 @@
 extends Node2D
 
+var obstacle := preload("res://src/obstacle.tscn")
 
 @onready var timer := $Timer
-@onready var obstacles := $ParallaxBackground/Obstacles
+@onready var obstacles_container := $ParallaxBackground/Obstacles
 
 
-func _process(delta: float) -> void:
-	pass
+func _ready() -> void:
+	timer.connect("timeout", self._create_obstacle)
 
 
-func _set_obstacle_position(obstacle) -> void:
+func _set_obstacle_position(instance) -> void:
 	var rand = randi_range(20, 80)
-	obstacle.global_position = Vector2(Globals.spawn_point, rand)
+	instance.global_position = Vector2(Globals.spawn_point, rand)
+
+
+func _create_obstacle() -> void:
+	var instance := obstacle.instantiate()
+	_set_obstacle_position(instance)
+	obstacles_container.add_child(instance)
--- src/obstacle.gd
+++ src/obstacle.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
+var speed := 100
 
-func _process(_delta: float) -> void:
+
+func _process(delta: float) -> void:
 	if global_position.x < Globals.despawn_point:
 		queue_free()
+
+	global_position.x = global_position.x - speed * delta
